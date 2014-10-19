95d5b2b0dabe4e27d9cd713d596a3d59b6f3dd47
Ricky <ricky@ubuntu.(none)>
2014-10-19 15:22:23 -0400

    created a class that represents  a packet. it stores only the information i need from the packets

--- flow.py
+++ flow.py
@@ -10,6 +10,13 @@ import sys
 import os
 from scapy.all import *
 
+class Packet:
+	def __init__(self, List):
+		self.proto = List[0]
+		self.src = List[1]
+		self.dst = List[2]
+		self.len = List[3]
+
 # Checks if the user wants to use a specific amout of packets (currently not using it)
 def checkOption():
 	if len(sys.argv[2]) != 0:
@@ -80,34 +87,55 @@ def checkProto(lista):
 		if protocol != lista[i].proto:
 			return False
 	return True
+
+def GetPacketInfo(packet):
+	pInfo = []
+	pInfo.append(packet.proto)
+	pInfo.append(packet["IP"].src)
+	pInfo.append(packet["IP"].dst)
+	pInfo.append(packet.len)
+	
+	return pInfo
+
  
 #-----------------------------------------------------
 if len(sys.argv) != 4:
 	sys.exit("ERROR: %s needs exactly 4 parameters:\n %s <file to read> <N packets> <output file>" %(sys.argv[0], sys.argv[0]))
 print("Reading files. Please wait...")
 #st = sys.argv[1] + "*" + ".pcap"
-a = []
 f = os.listdir("./")
 name = sys.argv[1]
 filteredList = list( filter((lambda x: name in x ), f))
-#contofFiles = 1
-for filename in filteredList:
-	x = rdpcap(filename)
-	a.extend(x)
-#	print "finished reading file", contofFiles
-#	contofFiles = contofFiles + 1
-print("Finished.")
-print
-print "Packets: ", len(a)
+del f # f is not needed any more, let us clear some memory
+
 M = {} # a dictionary to store different src, dst; represents a flow
+countofFiles = 1
+while len(filteredList):
+	if len(filteredList[0]) == 0:
+		continue
+	x = rdpcap(filteredList[0])
+	for i in range(len(x)):
+		packet = Packet(GetPacketInfo(x[i]))
+		sd = x[i]["IP"].dst + " - " + x[i]["IP"].src # string used as key for dictionary; dst + src
+		if sd in M:
+			M[sd].append(packet) # add a packet size to the corresponding flow
+		else:
+			M[sd] = [packet] # add to dictionary
+						   				# a list with the 
+										# packet of the new flow
+	del filteredList[0] #i wont need the file i just read so let me clear it from filteredlist
+	print "finished reading file", countofFiles
+	countofFiles += 1
+print("Finished.")
+
 
 # this for counts how many pairs of src / dst are there of each one
-for i in range(len(a)):
-	sd = a[i]["IP"].dst + " - " + a[i]["IP"].src # string used as key for dictionary; dst + src
-	if sd in M:
-		M[sd].append(a[i]) # add a packet to the corresponding flow
-	else:
-		M[sd] = [a[i]] # add to dictionary
+#for i in range(len(a)):
+#	sd = a[i]["IP"].dst + " - " + a[i]["IP"].src # string used as key for dictionary; dst + src
+#	if sd in M:
+#		M[sd].append(a[i]) # add a packet to the corresponding flow
+#	else:
+#		M[sd] = [a[i]] # add to dictionary
 					   # a list with the 
 					   # packet of the new flow
  
@@ -122,8 +150,8 @@ thefile = open(filename, "wb")
 
 for key, value in M.iteritems():
 	srcProto = value[0].proto
-	srcPortNum = value[0]["IP"].src
-	dstPortNum = value[0]["IP"].dst
+	srcPortNum = value[0].src
+	dstPortNum = value[0].dst
 	sizes = sizesOfNPackets(value)
 	maxSize = getMaxSize(sizes)
 	minSize = getMinSize(sizes)
